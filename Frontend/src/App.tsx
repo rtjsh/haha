29e639435a377cdbb79771febfeccdfe399fcf44
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-27 14:09:14 +0545

    hero section

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import Hero from "./components/Nav/Hero/Hero";
 import Nav from "./components/Nav/Nav";
 const App = () => {
   return (
-    <div className="min-h-full flex flex-col  items-center">
+    <div className="min-h-full flex flex-col  items-center py-6">
       <Nav />
+      <Hero />
     </div>
   );
 };
